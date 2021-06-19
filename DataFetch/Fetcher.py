570cef07066c6ea10bbe8ffe46e04a36d6e2b63c
AlexisG <alexis.gagnon.1@ens.etsmtl.ca>
2021-06-19 16:02:53 -0400

    updated

--- DataFetch/Fetcher.py
+++ DataFetch/Fetcher.py
@@ -14,33 +14,31 @@ Newline = "\r\n"
 """
 
 #I could make a program to look for the port.... but am I gonna?
-ser = serial.Serial('COM15', 57600, bytesize=serial.EIGHTBITS, timeout=30, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+ser = serial.Serial('COM16', 57600, bytesize=serial.EIGHTBITS, timeout=30.0, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser), newline='\r\n')
-
 
 #Fetch buffer
 def COM15_fetch():
-    msg = sio.readall()
+    msg = sio.readline()
     return msg
     sio.flush()
 
 #TALK TO ME LORA
 def Lora_setup():
-    ser.write("radio set freq 914000000")
+    ser.write(b'radio set freq 914000000')
     time.sleep(0.05)
-    ser.write("radio set sf sf11")
+    ser.write(b"radio set sf sf11")
     time.sleep(0.05)
-    ser.write("radio set bw 500")
+    ser.write(b"radio set bw 500")
     time.sleep(0.05)
-    ser.write("radio set sync bf")
+    ser.write(b"radio set sync bf")
     time.sleep(0.05)
-    ser.write("mac pause")
+    ser.write(b"mac pause")
 
 #It's action time babiiiii
-"""
+
 Lora_setup()
 
 while(1):
     raw_data = COM15_fetch()
     print(raw_data)
-"""
