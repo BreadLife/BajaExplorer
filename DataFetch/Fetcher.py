c96ee5545d06ebe3607ff07c112f1f3018c98b44
AlexisG <alexis.gagnon.1@ens.etsmtl.ca>
2021-07-01 22:24:41 -0400

    This fkn thing works now
    Thanks to Nic!

--- DataFetch/Fetcher.py
+++ DataFetch/Fetcher.py
@@ -14,50 +14,49 @@ Newline = "\r\n"
 """
 
 #I could make a program to look for the port.... but am I gonna?
-ser = serial.Serial('COM16', 57600, bytesize=serial.EIGHTBITS, timeout=30.0, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser), newline='\r\n')
+ser = serial.Serial('COM16', 57600, bytesize=serial.EIGHTBITS, timeout=30, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
 
 #Fetch buffer
-def COM15_fetch():
-    msg = sio.read()
-    if (msg != ""):
-        return msg
-    sio.flush()
+def COM_fetch():
+    msg = ser.read(size=57)
+    return msg
 
 #TALK TO ME LORA
 def Lora_setup():
-    ser.write(b'radio set freq 914000000')
-    time.sleep(0.05)
-    msg = sio.read()
+    ser.write(b'radio set freq 914000000\r\n')
+    time.sleep(0.1)
+    msg = ser.readline()
     print(msg)
-    ser.write(b'radio set sf sf11')
-    msg = sio.readlines()
+    ser.write(b'radio set sf sf11\r\n')
+    msg = ser.readline()
     print(msg)
-    time.sleep(0.05)
-    ser.write(b'radio set bw 500')
-    msg = sio.readlines()
+    time.sleep(0.1)
+    ser.write(b'radio set bw 500\r\n')
+    msg = ser.readline()
     print(msg)
-    time.sleep(0.05)
-    ser.write(b'radio set sync bf')
-    msg = sio.readlines()
+    time.sleep(0.1)
+    ser.write(b'radio set sync bf\r\n')
+    msg = ser.readline()
     print(msg)
-    time.sleep(0.05)
-    ser.write(b'mac pause')
-    msg = sio.readlines()
+    time.sleep(0.1)
+    ser.write(b'mac pause\r\n')
+    msg = ser.readline()
     print(msg)
-    time.sleep(0.05)
+    time.sleep(0.1)
 
 def test():
-    ser.write(b'AAAAAA')
-    time.sleep(0.05)
-    #msg = sio.read()
-    #print(msg)
+    ser.write(b'sys reset\r\n')
+    print(ser.readline())
+    time.sleep(1)
+    ser.write(b'sys get ver\r\n')
+    print(ser.readline())
 
 #It's action time babiiiii
-#Lora_setup()
-test()
+Lora_setup()
 
+#test()
 
 while(True):
-    raw_data = COM15_fetch()
+    ser.write(b'radio rx 100\r\n')
+    raw_data = COM_fetch()
     print(raw_data)
